16916ce2e05bff01a47e74021b696db91869b267
the4960 <cmurga4960@gmail.com>
2020-09-14 16:12:29 -0700

    Add files via upload
    python3

--- GeometryArt/GeomDownload.py
+++ GeometryArt/GeomDownload.py
@@ -7,7 +7,8 @@ Downloads geometry art from http://geometrydaily.tumblr.com/
 """
 import requests
 import urllib
-
+import urllib.request
+print('running...')
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
@@ -19,31 +20,35 @@ def find_between( s, first, last ):
 max = 38
 posts = []
 for page in range(1,max+1):
-    url = "http://geometrydaily.tumblr.com/page/"+str(page)
+    url = "https://geometrydaily.tumblr.com/page/"+str(page)
     response = requests.get(url)
     if response.status_code != 200:
-        print 'GOT ! 200'
+        print ('GOT ! 200')
         break
-    for line in response.content.split('\n'):
+    content = str(response.content)[1:-1]
+    for line in content.split('\n'):
         if 'href="' in line:
             splitter = line.split('href="')
             for elem in splitter:
-                if ('>' in elem) and 'http://geometrydaily.tumblr.com/' in elem:
+                if ('>' in elem) and 'https://geometrydaily.tumblr.com/' in elem:
                     elem = elem[:elem.index('">')]
-                    if elem.index('http://geometrydaily.tumblr.com/') == 0 and '/post/' in elem:
+                    print(elem)
+                    if elem.index('https://geometrydaily.tumblr.com/') == 0 and '/post/' in elem:
                         posts.append(elem)
     
 for page in posts:
     response = requests.get(page)
     if response.status_code != 200:
-        print 'GOT ! 200'
+        print ('GOT ! 200')
         break
-    for line in response.content.split('\n'):
+    for line in str(response.content)[1:-1].split('\n'):
         if '<div class="index"><div class="detail">' in line:
             splitter = line.split('<div class="index"><div class="detail">')
             elem = find_between( splitter[1],'src="','" style=')
             name = elem.split("/")[-1]
             print(elem+"\n")
-            urllib.urlretrieve(elem, "geom/"+name)
+            urllib.request.urlretrieve(elem, "geom/"+name)
+            
+            #urllib.urlretrieve(elem, "geom/"+name)
     
 print("END END END END")
